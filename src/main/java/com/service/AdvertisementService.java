0841357d10afbb759f9604644388e52ab250dd04
sasha <sashabogdan@ukr.net>
2016-08-10 15:35:07 +0300

    code refactoring

--- src/main/java/com/service/AdvertisementService.java
+++ src/main/java/com/service/AdvertisementService.java
@@ -26,44 +26,101 @@ public class AdvertisementService {
 	@Autowired
 	private UserDao userDao;
 
-	public void addAdvertisement(AdvertisementView advertisement) {
+	public void add(AdvertisementView advertisement) {
 		advertisementDao.add(viewToEntity(advertisement));
 	}
 
-	public AdvertisementView getAdvertisementById(int id) {
+	public AdvertisementView getById(int id) {
 		return new AdvertisementView(advertisementDao.get(id));
 	}
 
-	public void updateAdvertisement(AdvertisementView advertisement) {
+	public void update(AdvertisementView advertisement) {
 		advertisementDao.update(viewToEntity(advertisement));
 	}
 
-	public void deleteAdvertisement(AdvertisementView advertisement) {
+	public void delete(AdvertisementView advertisement) {
 		advertisementDao.delete(viewToEntity(advertisement));
 	}
 
-	@Transactional
-	public List<AdvertisementView> getAllByPrice(double min, double max) {
-		List<AdvertisementView> advertisements = new ArrayList<>();
-		for (Advertisement advertisement : advertisementDao.getAllByPrice(min, max)) {
-			advertisements.add(new AdvertisementView(advertisement));
-		}
-		return advertisements;
+	public List<AdvertisementView> getAllByPrice(double minPrice, double maxPrice) {
+		return listOfEntityToListOfView(advertisementDao.getAllByPrice(minPrice, maxPrice));
 	}
 
-	@Transactional
-	public List<AdvertisementView> getAllByPrice(int model, double min, double max) {
-		List<AdvertisementView> advertisements = new ArrayList<>();
-		for (Advertisement advertisement : advertisementDao.getAllByModelAndPrice(model, min, max)) {
-			advertisements.add(new AdvertisementView(advertisement));
-		}
-		return advertisements;
+	public List<AdvertisementView> getAllByModel(int modelId) {
+		return listOfEntityToListOfView(advertisementDao.getAllByModel(modelId));
 	}
 
-	@Transactional
-	public List<AdvertisementView> getAllAdvertisementByModel(int modelId) {
+	public List<AdvertisementView> getAllByModelAndPrice(int modelId, double minPrice, double maxPrice) {
+		return listOfEntityToListOfView(advertisementDao.getAllByModelAndPrice(modelId, minPrice, maxPrice));
+	}
+
+	public List<AdvertisementView> getAllByModelAndYear(int modelId, int minYear, int maxYear) {
+		return listOfEntityToListOfView(advertisementDao.getAllByModelAndYear(modelId, minYear, maxYear));
+	}
+
+	public List<AdvertisementView> getAllByModelAndCity(int modelId, int cityId) {
+		return listOfEntityToListOfView(advertisementDao.getAllByModelAndCity(modelId, cityId));
+	}
+
+	public List<AdvertisementView> getAllByModelAndUser(int modelId, int userId) {
+		return listOfEntityToListOfView(advertisementDao.getAllByModelAndUser(modelId, userId));
+	}
+
+	public List<AdvertisementView> getAllbyModelAndPriceAndYear(int modelId, double minPrice, double maxPrice,
+			int minYear, int maxYear) {
+		return listOfEntityToListOfView(
+				advertisementDao.getAllbyModelAndPriceAndYear(modelId, minPrice, maxPrice, minYear, maxYear));
+	}
+
+	public List<AdvertisementView> getAllbyModelAndPriceAndCity(int modelId, double minPrice, double maxPrice,
+			int cityId) {
+		return listOfEntityToListOfView(
+				advertisementDao.getAllbyModelAndPriceAndCity(modelId, minPrice, maxPrice, cityId));
+	}
+
+	public List<AdvertisementView> getAllbyModelAndPriceAndYearAndCity(int modelId, double minPrice, double maxPrice,
+			int minYear, int maxYear, int cityId) {
+		return listOfEntityToListOfView(advertisementDao.getAllbyModelAndPriceAndYearAndCity(modelId, minPrice,
+				maxPrice, minYear, maxYear, cityId));
+	}
+
+	public List<AdvertisementView> getAllByPriceAndYear(double minPrice, double maxPrice, int minYear, int maxYear) {
+		return listOfEntityToListOfView(advertisementDao.getAllByPriceAndYear(minPrice, maxPrice, minYear, maxYear));
+	}
+
+	public List<AdvertisementView> getAllByPriceAndCity(double minPrice, double maxPrice, int cityId) {
+		return listOfEntityToListOfView(advertisementDao.getAllByPriceAndCity(minPrice, maxPrice, cityId));
+	}
+
+	public List<AdvertisementView> getAllByPriceAndUser(double minPrice, double maxPrice, int userId) {
+		return listOfEntityToListOfView(advertisementDao.getAllByPriceAndUser(minPrice, maxPrice, userId));
+	}
+
+	public List<AdvertisementView> getAllbyPriceAndYearAndCity(double minPrice, double maxPrice, int minYear,
+			int maxYear, int cityId) {
+		return listOfEntityToListOfView(
+				advertisementDao.getAllbyPriceAndYearAndCity(minPrice, maxPrice, minYear, maxYear, cityId));
+	}
+
+	public List<AdvertisementView> getAllByYear(int minYear, int maxYear) {
+		return listOfEntityToListOfView(advertisementDao.getAllByYear(minYear, maxYear));
+	}
+
+	public List<AdvertisementView> getAllByYearAndCity(int minYear, int maxYear, int cityId) {
+		return listOfEntityToListOfView(advertisementDao.getAllByYearAndCity(minYear, maxYear, cityId));
+	}
+
+	public List<AdvertisementView> getAllByCity(int cityId) {
+		return listOfEntityToListOfView(advertisementDao.getAllByCity(cityId));
+	}
+
+	public List<AdvertisementView> getAllByUser(int userId) {
+		return listOfEntityToListOfView(advertisementDao.getAllByUser(userId));
+	}
+
+	private List<AdvertisementView> listOfEntityToListOfView(List<Advertisement> entities) {
 		List<AdvertisementView> advertisements = new ArrayList<>();
-		for (Advertisement advertisement : advertisementDao.getAllByModel(modelId)) {
+		for (Advertisement advertisement : entities) {
 			advertisements.add(new AdvertisementView(advertisement));
 		}
 		return advertisements;
@@ -79,20 +136,5 @@ public class AdvertisementService {
 		entityAdvertisement.setUser(userDao.get(advertisement.getUser().getId()));
 		return entityAdvertisement;
 	}
-	//
-	// private AdvertisementView entityToView(Advertisement advertisement) {
-	// AdvertisementView viewAdvertisement = new AdvertisementView();
-	// viewAdvertisement.setId(advertisement.getId());
-	// viewAdvertisement.setDescription(advertisement.getDescription());
-	// viewAdvertisement.setPrice(advertisement.getPrice());
-	// viewAdvertisement.setModel(new ModelView(advertisement.getModel()));
-	// List<PhotoView> photos = new ArrayList<>();
-	// for (Photo photo : advertisement.getPhotos()) {
-	// photos.add(new PhotoView(photo));
-	// }
-	// viewAdvertisement.setPhotos(photos);
-	// viewAdvertisement.setUser(new UserView(advertisement.getUser()));
-	// return viewAdvertisement;
-	// }
 
 }
