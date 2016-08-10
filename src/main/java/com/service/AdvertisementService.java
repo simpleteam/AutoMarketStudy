aeb8535242f739e9683a3b980a396db18c08f252
sasha <sashabogdan@ukr.net>
2016-08-10 14:57:27 +0300

    add code

--- src/main/java/com/service/AdvertisementService.java
+++ src/main/java/com/service/AdvertisementService.java
@@ -1,6 +1,5 @@
 package com.service;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Transactional
 public class AdvertisementService {
 
 	@Autowired
@@ -43,34 +43,27 @@ public class AdvertisementService {
 	}
 
 	@Transactional
-	public List<AdvertisementView> getAllByPrice(double min, double max){
-		
+	public List<AdvertisementView> getAllByPrice(double min, double max) {
 		List<AdvertisementView> advertisements = new ArrayList<>();
-		for(Advertisement advertisement : advertisementDao.getAllByPrice(min, max)){
+		for (Advertisement advertisement : advertisementDao.getAllByPrice(min, max)) {
 			advertisements.add(new AdvertisementView(advertisement));
 		}
-		
 		return advertisements;
 	}
-	
-	
+
 	@Transactional
-	public List<AdvertisementView> getAllByPrice(int model, double min, double max){
-		
+	public List<AdvertisementView> getAllByPrice(int model, double min, double max) {
 		List<AdvertisementView> advertisements = new ArrayList<>();
-		for(Advertisement advertisement : advertisementDao.getAllByModelAndPrice(model, min, max)){
+		for (Advertisement advertisement : advertisementDao.getAllByModelAndPrice(model, min, max)) {
 			advertisements.add(new AdvertisementView(advertisement));
 		}
-		
 		return advertisements;
 	}
-	
-	
-	
+
 	@Transactional
 	public List<AdvertisementView> getAllAdvertisementByModel(int modelId) {
 		List<AdvertisementView> advertisements = new ArrayList<>();
-		for(Advertisement advertisement : advertisementDao.getAllByModel(modelId)){
+		for (Advertisement advertisement : advertisementDao.getAllByModel(modelId)) {
 			advertisements.add(new AdvertisementView(advertisement));
 		}
 		return advertisements;
@@ -79,26 +72,27 @@ public class AdvertisementService {
 	private Advertisement viewToEntity(AdvertisementView advertisement) {
 		Advertisement entityAdvertisement = new Advertisement();
 		entityAdvertisement.setId(advertisement.getId());
+		entityAdvertisement.setYear(advertisement.getYear());
 		entityAdvertisement.setPrice(advertisement.getPrice());
 		entityAdvertisement.setDescription(advertisement.getDescription());
 		entityAdvertisement.setModel(modelDao.get(advertisement.getModel().getId()));
 		entityAdvertisement.setUser(userDao.get(advertisement.getUser().getId()));
 		return entityAdvertisement;
 	}
-//
-//	private AdvertisementView entityToView(Advertisement advertisement) {
-//		AdvertisementView viewAdvertisement = new AdvertisementView();
-//		viewAdvertisement.setId(advertisement.getId());
-//		viewAdvertisement.setDescription(advertisement.getDescription());
-//		viewAdvertisement.setPrice(advertisement.getPrice());
-//		viewAdvertisement.setModel(new ModelView(advertisement.getModel()));
-//		List<PhotoView> photos = new ArrayList<>();
-//		for (Photo photo : advertisement.getPhotos()) {
-//			photos.add(new PhotoView(photo));
-//		}
-//		viewAdvertisement.setPhotos(photos);
-//		viewAdvertisement.setUser(new UserView(advertisement.getUser()));
-//		return viewAdvertisement;
-//	}
+	//
+	// private AdvertisementView entityToView(Advertisement advertisement) {
+	// AdvertisementView viewAdvertisement = new AdvertisementView();
+	// viewAdvertisement.setId(advertisement.getId());
+	// viewAdvertisement.setDescription(advertisement.getDescription());
+	// viewAdvertisement.setPrice(advertisement.getPrice());
+	// viewAdvertisement.setModel(new ModelView(advertisement.getModel()));
+	// List<PhotoView> photos = new ArrayList<>();
+	// for (Photo photo : advertisement.getPhotos()) {
+	// photos.add(new PhotoView(photo));
+	// }
+	// viewAdvertisement.setPhotos(photos);
+	// viewAdvertisement.setUser(new UserView(advertisement.getUser()));
+	// return viewAdvertisement;
+	// }
 
 }
