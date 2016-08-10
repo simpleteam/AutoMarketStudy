2b8241ee5c7136added0845173a33b3a31841c5d
sasha <sashabogdan@ukr.net>
2016-08-10 15:54:16 +0300

    bug fixing

--- src/main/java/com/mvc/AdvertisementController.java
+++ src/main/java/com/mvc/AdvertisementController.java
@@ -16,18 +16,13 @@ import com.service.AdvertisementService;
 import com.service.MarketService;
 import com.view.AdvertisementView;
 
-
-
 @Controller
 @RequestMapping("/advertisement")
 public class AdvertisementController {
-	
+
 	@Autowired
 	private MarketService service;
-	
-	@Autowired
-	private AdvertisementDao advertisementDao;
-	
+
 	@Autowired
 	private AdvertisementService advertisementService;
 
@@ -44,54 +39,42 @@ public class AdvertisementController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String processCreatingAdvertisement(AdvertisementView advertisement, BindingResult bindingResult, Model model) {
+	public String processCreatingAdvertisement(AdvertisementView advertisement, BindingResult bindingResult,
+			Model model) {
 		if (bindingResult.hasErrors()) {
 			model.addAttribute("advertisement", advertisement);
 			return "/advertisement/initCreateAdvertisement";
 		}
-
-		service.addAdvertisement(advertisement);;
-		
-	
-
+		service.addAdvertisement(advertisement);
+		;
 		model.addAttribute("advertisement", advertisement);
-
-		// regionDao.add(region);
 		return "redirect:/advertisement/" + advertisement.getId();
 	}
-	
+
 	@RequestMapping(value = "/getAll", method = RequestMethod.GET)
 	@ResponseBody
-	public List<AdvertisementView> getAllAdvertisements(){
+	public List<AdvertisementView> getAllAdvertisements() {
 		return service.getAllAdvertisement();
 	}
-	
+
 	@RequestMapping(value = "/getAllById/{id}", method = RequestMethod.GET)
 	@ResponseBody
-	public List<AdvertisementView> getAdvertisementsByModel(@PathVariable("id")int modelId){
-		return advertisementService.getAllAdvertisementByModel(modelId);
+	public List<AdvertisementView> getAdvertisementsByModel(@PathVariable("id") int modelId) {
+		return advertisementService.getAllByModel(modelId);
 	}
-	
+
 	@RequestMapping(value = "/getAll/{min}/{max}", method = RequestMethod.GET)
 	@ResponseBody
-	public List<AdvertisementView> getAdvertisementsByModel(@PathVariable("min")int min, @PathVariable("max")int max){
+	public List<AdvertisementView> getAdvertisementsByModel(@PathVariable("min") int min,
+			@PathVariable("max") int max) {
 		return advertisementService.getAllByPrice(min, max);
 	}
-	
+
 	@RequestMapping(value = "/getAll/{min}/{max}/{model}", method = RequestMethod.GET)
 	@ResponseBody
-	public List<AdvertisementView> getAdvertisementsByModel(@PathVariable("min")int min, @PathVariable("max")int max, @PathVariable("model")int model){
-		return advertisementService.getAllByPrice(model, min, max);
+	public List<AdvertisementView> getAdvertisementsByModel(@PathVariable("min") int min, @PathVariable("max") int max,
+			@PathVariable("model") int model) {
+		return advertisementService.getAllByModelAndPrice(model, min, max);
 	}
-	
-	
 
-//	@RequestMapping(value = "/getAll", method = RequestMethod.GET)
-//	@ResponseBody
-//	public List<RegionView> getAllRegions() {
-//		return service.getAllRegions();
-//	}
-	
-	
-	
 }
